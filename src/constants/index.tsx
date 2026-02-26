b63ca8d1cb16952d1c8fcad5b4de7dc775807dac
Harman Muhammad Satrio Reinaldy <harmanmsr@gmail.com>
2026-02-27 00:48:57 +0700

    implement rest data provider

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,8 +1,22 @@
 export const DEPARTMENTS = [
-    'CS', 'Math', 'English'
+    'CS', 
+    'Math', 
+    'English'
 ];
 
 export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
     value: dept,
     label: dept,
 }));
+
+export const CLOUDINARY_UPLOAD_URL = import.meta.env.VITE_CLOUDINARY_UPLOAD_URL;
+export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
+
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const ACCESS_TOKEN_KEY = import.meta.env.VITE_ACCESS_TOKEN_KEY;
+export const REFRESH_TOKEN_KEY = import.meta.env.VITE_REFRESH_TOKEN_KEY;
+
+export const REFRESH_TOKEN_URL = `${BASE_URL}/refresh-token`;
+
+export const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
